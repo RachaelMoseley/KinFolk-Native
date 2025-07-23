53c1e37d66e2277188a6a240707f7120ee414bcb
Student <student@Admins-MacBook-Pro.local>
2025-07-23 11:22:52 -0400

    Updated

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,1 +1,43 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
+export default function App() {
+    return (
+        <View style={StyleSheet.container}>
+            <View style={StyleSheet.header}>
+                <Text style={StyleSheet.headerText}>Welcome to KinFolk!</Text>
+            </View>
+
+
+        </View>
+
+    );
+
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'space-between',
+        padding: 20,
+        backgroundColor: '50a7c1',
+    },
+
+    header: {
+        backgroundColor: '0a7ea4',
+        padding: 15,
+        borderRadius: 5,
+    },
+
+    headerText: {
+        color: 'white',
+        fontSize: 24,
+        fontWeight: 'bold',
+    },
+
+    content:  {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+});
