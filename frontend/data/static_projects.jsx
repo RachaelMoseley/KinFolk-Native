1f4839a7e47c15cc892e95546c9a304a777819f2
Student <student@Admins-MacBook-Pro.local>
2025-07-23 10:55:59 -0400

    fixed one error

---
+++ frontend/data/static_projects.jsx
@@ -0,0 +1,50 @@
+export const static_projects = [
+    {
+        projectId: "10",
+        title: "The Story of John",
+        location: "Detroit, MI"
+
+    },
+
+    {
+        projectId: "11",
+        title: "A Short Film",
+        location: "Detroit, MI"
+    },
+
+    {
+        projectId: "12",
+        title: "Dancers Era",
+        location: "Detroit, MI"
+    },
+
+    {
+        projectId: "13",
+        title: "Wow Momement",
+        location: "Detroit, MI"
+    },
+
+    {
+    projectId: "14",
+    title: "ArtIS ME",
+    location: "NewYork, NY"
+    },
+
+    {
+    projectId: "15",
+    title: "Avacado",
+    location: "NewYork, NY"
+    },
+
+    {
+    projectId: "16",
+    title: "Daily LifeStyle",
+    location: "NewYork, NY"
+    },
+
+    {
+    projectId: "17",
+    title: "FlowerUP",
+    location: "NewYork, NY"
+    }
+];
---
+++ frontend/data/static_users.jsx
@@ -0,0 +1,92 @@
+export const static_users = [
+    {
+        userId: "1",
+        fullName: "John Doe",
+        role: "Film Maker",
+        field: "Film Making",
+        location: "Detroit, MI",
+        skills: ["Directing", "Editing", "Coordinator", "Leader"],
+        experience: "Experienced",
+        priceRange: "$100-$5000"
+    },
+
+    {
+        userId: "2",
+        fullName: "Tanya Smith",
+        role: "Content Creator",
+        field: "Media Arts",
+        location: "Detroit, MI",
+        skills: ["Editing", "Actor", "Producer", "Innovator"],
+        experience: "Experienced",
+        priceRange: "$500-$1500"
+
+    },
+
+    {
+        userId: "3",
+        fullName: "Jessie Duran",
+        role: "Content Creator",
+        field: "Media Arts",
+        location: "Detroit, MI",
+        skills: ["Editing", "Creative", "Producer"],
+        experience: "Somewhat Experienced",
+        priceRange: "$75-500"
+    },
+
+    {
+        userId: "4",
+        fullName: "Maha Udin",
+        role: "Film Director",
+        field: "Film Making",
+        location: "NewYork, NY",
+        skills: ["Editing", 'Coordinator', 'Leader', "Producer"],
+        experience: "Well Experienced",
+        priceRange: "$2000-$5000"
+
+
+    },
+
+    {
+        userId: "5",
+        fullName: "Cory Smith",
+        role: "Painter",
+        field: "Painting",
+        location: "NewYork, NY",
+        skills: ["Painting", "Creative"],
+        experience: "Somewhat Experienced",
+        priceRange: "$150-$500"
+    },
+
+    { 
+        userId: "6",
+        fullName: "Anna Hill",
+        role: "Painter",
+        field: "Painting",
+        location: "Detroit, MI",
+        skills: ["Painting", "Creative"],
+        experience: "Experienced",
+        priceRange: "$500-$2000"
+    },
+
+    {
+        userId: "7",
+        fullName: "Jackson Jones",
+        role: "Flim Director",
+        field: "Film Making",
+        location: "Detroit, MI",
+        skills: ["Film Maker", "Creative", "Leader", "Directing"],
+        experience: "Somewhat Experienced",
+        priceRange: "$150-$500"
+    },
+
+    {
+        userId: "8",
+        fullName: "Santana McCould",
+        role: "Performer",
+        field: "Performing Arts",
+        location: "Detroit, MI",
+        skills: ["Dancer", "Creative", "Actor", "Performer"],
+        experience: "Experienced",
+        priceRange: "$500-$1000"
+    }
+];
